2fb4f4bc6243fbfed26adc888f28be44b3a369fa
Nikolay.Baryshnikov <nkolay.baryshnikov@uklon.com.ua>
2026-02-17 11:14:22 +0100

    update test descr

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,13 +1,14 @@
 import {test,expect} from '@playwright/test'
 
-test.only('has title',async ({page})=>{
+test.describe('Search Tests', () => {
+  test('has title',async ({page})=>{
     await page.goto('https://playwright.dev/');
 
     await expect(page).toHaveTitle(/Playwright/);
 
 })
 
-test.only('has link', async({page})=>{
+  test('has link', async({page})=>{
     await page.goto('https://playwright.dev/')
 
     await page.getByRole('link',{name:'Get started'}).click();
@@ -17,10 +18,11 @@ test.only('has link', async({page})=>{
 
 
 
-test.only('test', async ({ page }) => {
+ test('test', async ({ page }) => {
   await page.goto('https://gruppenplatz.healthycloud.de/HC_GP_Public_Pages/');
   await page.getByRole('button', { name: 'Accept All' }).click({ timeout: 10000, force: true });
   await page.getByRole('combobox', { name: 'Bitte Ort oder Postleitzahl' }).click();
   await page.getByRole('combobox', { name: 'Bitte Ort oder Postleitzahl' }).fill('Dresden');
   await page.getByRole('button', { name: 'Gruppen suchen' }).click();
+});
 });
